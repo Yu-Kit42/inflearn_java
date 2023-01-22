791b4ccdfd1e5a398a3ea27a12fead4ed9bb8f1b
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-22 10:56:21 +0900

    I-10
    강의 안보고

---
+++ src/I_10.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class I_10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String tp1 = bf.readLine();
+        char[] arr = tp1.split(" ")[0].toCharArray();
+        char t = tp1.split(" ")[1].charAt(0);
+
+        boolean[] chArr = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == t) chArr[i] = true;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int k = 0; k < arr.length; k++) {
+                if (i - k >= 0){
+                    if (chArr[i - k]) {
+                        System.out.print(k + " ");
+                        break;
+                    }
+                }
+                if(i + k < arr.length){
+                    if (chArr[i + k]) {
+                        System.out.print(k + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

39c9faff84c983f96fe900a2642b7a8b02c8c72a
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-22 12:03:27 +0900

    I_02_02
    보이는 학생

---
+++ src/I_02_02.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class I_02_02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        String[] tp1 = bf.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
+        int max = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (Integer.parseInt(tp1[i]) > max){
+                max = Integer.parseInt(tp1[i]);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+

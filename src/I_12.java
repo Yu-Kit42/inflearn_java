366310a07089bdd6fd13ae69715f9684be1865fb
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-22 11:44:06 +0900

    I_12
    암호
    대충 #**#을 입력받아 2진수로 바꾸고 아스키코드값으로 변환해서 출력하기

---
+++ src/I_12.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class I_12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        String tp1 = bf.readLine();
+        String out = "";
+        String tp = "";
+
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < 7; k++){
+                tp += (tp1.charAt(i * 7 + k) == '#' ? 1 : 0);
+            }
+            out += (char) (Integer.parseInt(tp, 2));
+            tp = "";
+        }
+        System.out.println(out);
+    }
+}
+

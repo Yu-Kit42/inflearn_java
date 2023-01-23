08f13ad757e671539ba6f1d600b4ce5d4bd080a5
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-23 10:52:27 +0900

    I_02_05
    에라토스테네의 체

---
+++ src/I_02_05.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class I_02_05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        boolean[] arr = new boolean[N + 1];
+
+        arr[0] = true;
+        arr[1] = true;
+        int cnt = 0;
+
+        for (int i = 2; i <= N; i++) {
+            if (arr[i]) continue;
+            else {
+                cnt++;
+                for (int k = i; k <= N; k += i) arr[k] = true;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+

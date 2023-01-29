ef8e76d5a904b4e727e3d23ee35185678ab1b832
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-29 14:56:24 +0900

    I_05_04
    후위 표기식

---
+++ src/I_05/I_05_04.java
@@ -0,0 +1,40 @@
+package I_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class I_05_04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        char[] temp = bf.readLine().toCharArray();
+        Stack<Integer> stack = new Stack<>();
+
+
+        for (char c : temp) {
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int p2 = stack.pop();
+                int p1 = stack.pop();
+                switch (c) {
+                    case '+':
+                        stack.push(p1 + p2);
+                        break;
+                    case '-':
+                        stack.push(p1 - p2);
+                        break;
+                    case '*':
+                        stack.push(p1 * p2);
+                        break;
+                    case '/':
+                        stack.push(p1 / p2);
+                        break;
+                }
+            }
+        }
+        System.out.println(stack.pop());
+    }
+}
+

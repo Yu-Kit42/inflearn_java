e0ab8917d038c892c641d3aa87ef7dee601d6897
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-29 16:08:32 +0900

    I_05_05
    쇠막대기

---
+++ src/I_05/I_05_05.java
@@ -0,0 +1,32 @@
+package I_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class I_05_05 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        char[] temp = bf.readLine().toCharArray();
+        Stack<Character> stack = new Stack<>();
+        int cnt = 0;
+        for (int i = 0; i < temp.length; i++) {
+            if (!stack.isEmpty() && temp[i] == ')') {
+                if (stack.peek() == '(' && temp[i - 1] != ')') {
+                    stack.pop();
+                    cnt += stack.size();
+                } else {
+                    stack.pop();
+                    cnt++;
+                }
+//                System.out.println(cnt);
+            } else {
+                stack.push(temp[i]);
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+

beebf44fea5060c4560e11634e69d4640d9e87e8
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-31 20:47:27 +0900

    I_05_08

--- src/I_05/I_05_08.java
+++ src/I_05/I_05_08.java
@@ -3,28 +3,59 @@ package I_05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Person {
+    int id;
+    int p;
+
+    public Person(int id, int p) {
+        this.id = id;
+        this.p = p;
+    }
+}
 
 public class I_05_08 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        char[] temp = bf.readLine().toCharArray();
-//        int N = Integer.parseInt(temp[0]);
-//        int M = Integer.parseInt(temp[1]);
-//        String[] tp1 = bf.readLine().split(" ");
-//        String[] tp2 = bf.readLine().split(" ");
-        StringBuilder sb = new StringBuilder();
-        int[] arr = new int[26];
-        int max = 0;
-        int idx = 0;
+        String[] tp1 = bf.readLine().split(" ");
+        int N = Integer.parseInt(tp1[0]);
+        int M = Integer.parseInt(tp1[1]);
+        String[] tp2 = bf.readLine().split(" ");
+        int[] max = new int[6];
+        int maxTp = 0;
+
+        Queue<Person> q = new LinkedList<>();
+
 
         for (int i = 0; i < N; i++) {
-            if (++arr[temp[i] - 65] > max) {
-                max = ++arr[temp[i] - 65];
-                idx = temp[i];
-            }
+            int p = Integer.parseInt(tp2[i]);
+            q.offer(new Person(i, p));
+            max[p / 10 - 5] += 1;
+            maxTp = Math.max(maxTp, p / 10 - 5);
         }
-        System.out.println((char) idx);
+
+        int cnt = 0;
+        int tp = 0;
+
+        while (true) {
+            if (max[maxTp] == 0) {
+                maxTp -= 1;
+                continue;
+            }
+
+            for (Person i : q) System.out.print(i.p + " ");
+            System.out.printf(" 인덱스:%d max:%d 개수:%d %d\n", q.peek().id, maxTp, max[maxTp], q.peek().p / 10 - 5);
+
+            if (q.peek().p / 10 - 5 == maxTp) {
+                cnt++;
+                if (q.poll().id == M) break;
+                max[maxTp] -= 1;
+            } else q.offer(q.poll());
+
+        }
+        System.out.println(cnt);
     }
 }
 

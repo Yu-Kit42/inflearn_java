210005a6fd3e5e08963842091c2ca9529b985d45
Yu-Kit42 <thdwnghks121@gmail.com>
2023-01-31 19:26:58 +0900

    I_05_8

---
+++ src/I_05/I_05_08.java
@@ -0,0 +1,30 @@
+package I_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class I_05_08 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        char[] temp = bf.readLine().toCharArray();
+//        int N = Integer.parseInt(temp[0]);
+//        int M = Integer.parseInt(temp[1]);
+//        String[] tp1 = bf.readLine().split(" ");
+//        String[] tp2 = bf.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[26];
+        int max = 0;
+        int idx = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (++arr[temp[i] - 65] > max) {
+                max = ++arr[temp[i] - 65];
+                idx = temp[i];
+            }
+        }
+        System.out.println((char) idx);
+    }
+}
+
